d259b0cf5da49ad7ca7fee6d190daa90033cf143
smannaba <74249644+rahmads773@users.noreply.github.com>
2022-09-25 22:16:26 +0100

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,60 @@
+// animation on scroll and on load
+
+
+let landingTexts =  [document.querySelector(".landing .text .h1-con span"),document.querySelector(".landing .text .p-con span")]
+let landingTitles =  [document.querySelector(".landing .text .h1-con"),document.querySelector(".landing .text .p-con"),document.querySelector(".landing .text a")]
+let landingImg =  document.querySelector(".landing .image img")
+let title = document.querySelectorAll(".main-title")
+let pills =  document.querySelectorAll(".main-title i")
+let about =  document.querySelector(".about")
+let aboutImg = document.querySelector(".about .image img")
+let aboutSpan = document.querySelectorAll(".about .content .p-about span")
+let aboutText = document.querySelector(".about .content")
+let noteBoxes =  document.querySelectorAll(".notes .box")
+// let loader =  document.querySelector(".loader")
+
+const come = (item) => { 
+    item.style.transform = "translate(0)"
+    item.style.opacity = "1"
+ }
+
+
+window.onload = (eo) => { 
+    loader.classList.add("active")
+setTimeout(() => { loader.style.display = "none" },6000)
+header.style.transform =  "translate(0)"
+if(landingTexts){
+    landingTexts.forEach((item) => { 
+        item.style.width = "0"
+     })
+}
+ landingTitles.forEach((ele) => { 
+    come(ele)
+  })
+ come(landingImg)
+}
+
+window.onscroll = () => { 
+
+
+title.forEach((ele) => { 
+    if(window.scrollY >= ele.parentElement.offsetTop - 500){
+        come(ele)
+            }
+ })
+
+if(window.scrollY >= about.offsetTop - 300){
+come(aboutImg)
+aboutSpan.forEach((ele) => { 
+    ele.style.width = "0"
+ })
+come(aboutText)
+}
+
+noteBoxes.forEach(ele => {
+    if(window.scrollY >= ele.offsetTop - 550){
+   come(ele)
+    }    
+});
+
+ }
---
+++ main.js
@@ -0,0 +1,99 @@
+
+// fade
+const fade = (ele) => { 
+    ele.classList.toggle("active")
+ }
+
+// toggle menu
+let menu = document.querySelector(".menu i")
+let overlay = document.querySelector(".overlay")
+menu.addEventListener("click",(eo) => { 
+    let list = `
+    <i class="fa-solid fa-xmark"></i>
+    <ul>
+    <li><a href="index.html" class="link">Home</a></li>
+    <li><a href="#about" class="link">About</a></li>
+    <li><a href="#notes" class="link">Notes</a></li>
+    <li><a href="#contact" class="link">Contact</a></li>
+</ul>
+<i class="fa-solid fa-pills"></i>
+    `
+      let listSecond = `
+    <i class="fa-solid fa-xmark"></i>
+    <ul>
+    <li><a href="index.html" class="link">Home</a></li>
+    <li><a href="index.html#about" class="link">About</a></li>
+    <li><a href="index.html#notes" class="link">Notes</a></li>
+    <li><a href="index.html#contact" class="link">Contact</a></li>
+</ul>
+<i class="fa-solid fa-pills"></i>
+    `
+    if(overlay.classList.contains("secondary")){
+        overlay.innerHTML = listSecond
+    }else{
+        overlay.innerHTML = list
+    }
+    
+    overlay.classList.add("active")
+
+ })
+
+ overlay.addEventListener("click",(eo) => { 
+    overlay.classList.remove("active")
+  })
+
+
+
+//   toggle list
+
+let mainItems =  document.querySelectorAll("main .list .main-item")
+let courseLists =  document.querySelectorAll("main .list .main-item .course-list")
+let arrows =  document.querySelectorAll("main .list .main-item i")
+
+mainItems.forEach((item) => {
+    item.addEventListener("click",(eo) => {     
+let ele = eo.target.parentElement.getElementsByClassName("course-list")[0]
+    ele.classList.toggle("active")
+let arrow = eo.target.parentElement.querySelector("i")
+
+    if(ele.classList.contains("active")){
+        ele.style.height = `${ele.scrollHeight}px`
+        arrow.style.transform = "rotate(180deg)"
+        
+    }else{
+        ele.style.height = `0`
+        arrow.style.transform = "rotate(0)"
+    }
+    
+
+     })
+});
+
+
+
+// onload animation
+
+let header =  document.querySelector("header")
+let mainTitle = document.querySelector("main .main-title")
+let mainItem =  document.querySelectorAll("main .list .main-item")
+let loader =  document.querySelector(".loader")
+
+for (let i = 0; i < mainItem.length; i++) {
+    mainItem[i].style.transitionDelay += `${i / 10}s`
+    
+}
+
+window.onload = (eo) => { 
+    loader.classList.add("active")
+    header.style.transform =  "translate(0)"
+    mainTitle.style.opacity = "1"
+    mainTitle.style.transform = `translate(0)`
+    mainItem.forEach(ele => {
+        ele.style.opacity ="1"
+        ele.style.transform = "translate(0)"
+        
+    });
+   
+    }
+
+ 
